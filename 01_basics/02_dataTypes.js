9c3a285ab59e62a3907cc4f76ab6548942eeaf79
SaddamHusain2005 <155045186+SaddamHusain2005@users.noreply.github.com>
2023-12-31 06:44:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 
+    + 3) // code readability should be high
+
+    console.log("saddam")
+
+    // for documentation use always tc39.es & mdn web docs
+
+    let name = "saddam"
+    let age = 18
+    let isloggedin = false
+
+    // these are our Premative dataTypes. Line no.18 to 24
+    
+    // number => 2 to the power 53
+    // bigint => if our number more than big then we use bigint.
+    // string => "" we should use it in double code.
+    // bolean => here we get only to value firt/true second/false we have to say that user logged in or not and we have to say that user crdit card detail available or not in these cause boolean will be use.
+    // null=> standalone value/ it is a representation of empty value.
+    // undefined=> iska matlab avi value defined nahi hui hai.
+    // symbol => uniqueness
+
+
+    // object
+
+    console.log(typeof age); // it type alway print age.
+    console.log(typeof undefined); // type always print undefined.
+    console.log(typeof  null); // it type always print Object.
+
+    
