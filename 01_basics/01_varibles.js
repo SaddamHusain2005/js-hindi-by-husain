96292a3110972c88d8a2b66439d48f2ebf184380
SaddamHusain2005 <155045186+SaddamHusain2005@users.noreply.github.com>
2023-12-31 06:52:03 +0000

    variables

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,18 @@
+const accountID = 131313
+let accountEmail = "saddamsiddiqui98275@gmail.com";
+var accountPassword = "324355"
+accountCity =" jaipur"
+let accountStates;
+
+// accoountId =2 // not allowed
+
+
+accountEmail = "officialhusain@gmail.accountEmail"
+accountPassword ="2121212"
+accountCity = "bengluru"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountStates]);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
