e8e456dc181a6f13dd55b9ffbe02b1abc9ac8e56
SaddamHusain2005 <155045186+SaddamHusain2005@users.noreply.github.com>
2024-01-01 05:49:25 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+//console.log(2 > 1); //two greater than one// its value always will come in boolean values/
+//console.log(2 >= 1); //two greater than one or equal to one// its value always will come in boolean values/
+//console.log(2 < 1); // tow less than one// its value always will come in boolean value.
+//console.log(2 = 1); // two equal to one// its value always will come in boolean value/
+//console.log(2 != 1); // two not equal two one// its value always will come in boolean value/
+
+// most of the cause main hum below bale compariosn ko avoid karte hain is Tarha ke conversion kabhi bhi aapko confuseing mai daal sakte hain/
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+//strict check
+// === // ye value ko strictly check karta hai// or ye dataType ko bhi check karta hai
+
+console.log("2" === 2); //
